ea15a1f1af84e38aefaf7afcabdd1b6982175bc3
Denis B <denis@butko.info>
2024-10-03 00:33:58 +0300

    fix version

--- src/muscles/asgi/__about__.py
+++ src/muscles/asgi/__about__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-__name__ = "Muscular CLI Framework asgi"
-__version__ = "0.2.0.dev"
+__name__ = "Muscular CLI Framework: asgi"
+__version__ = "0.0.3"
 __description__ = (
     "Muscular CLI Framework"
 )
